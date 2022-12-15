86df0d7aed07ed993b635c46e5592588ac075af2
sagar <nandipatisagar9652@gmail.com>
2022-12-15 13:47:51 +0530

    add

---
+++ grade.py
@@ -0,0 +1,9 @@
+#program5
+mark=int(input("enter the marks b/w 0-100:"))
+if mark>=90:print("grade:o")
+elif 90>mark>=80:print("grade:A+")
+elif 80>mark>=70:print("grade:A")
+elif 70>mark>=60:print("grade:B+")
+elif 60>mark>=50:print("grade:B")
+elif 50>mark>=40:print("grade:C")
+else:print("student is fail")
